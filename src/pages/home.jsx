320011a8c708fa944eaba1b4db41ebef2cedabd4
im-nihar <niharsonawane@gmail.com>
2023-06-19 23:23:54 +0100

    Minor Changes

---
+++ src/pages/home.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+
+export default function Home() {
+  const daysList = [
+    { day: "Day 0", msg: "She Left" },
+    {
+      day: "Day 1",
+      msg: "First Day without Her",
+      dayWent: "Did Shopping, ate lunch, went for a walk",
+    },
+    { day: "Day 2", msg: "" },
+    { day: "Day 3", msg: "" },
+  ];
+
+  useEffect(() => {
+    console.log("daysList", daysList);
+  }, []);
+  return (
+    <div>
+      {daysList.map((element, index) => (
+        <div key={index}>
+          <h2>{element.day}</h2>
+          <h3>{element.msg}</h3>
+          <h3>{element.dayWent}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
